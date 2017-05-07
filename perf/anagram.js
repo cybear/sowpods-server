215a107a45dd3ddee4919fa9d998b86785cb9832
Björn Söderqvist <bjorn@greenfield.se>
2017-05-07 21:56:44 +0200

    Basic performance benchmarking

---
+++ perf/anagram.js
@@ -0,0 +1,25 @@
+const Benchmark = require('benchmark');
+const sowpods = require('sowpods');
+const anagram = require('../lib/anagram')(sowpods);
+
+const suite = new Benchmark.Suite;
+
+// add tests
+suite.add('`for`', function() {
+  const myWord = 'AELST';
+  const results = sowpods.filter(word => {
+    const key = word.split('').sort().join('');
+    return key === myWord;
+  });
+})
+.add('sorted letter index', function() {
+  const myWord = 'AELST';
+  const results = anagram[myWord];
+})
+.on('cycle', function(event) {
+  console.log(String(event.target));
+})
+.on('complete', function() {
+  console.log('Fastest is ' + this.filter('fastest').map('name'));
+})
+.run();
