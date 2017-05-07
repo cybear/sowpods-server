39f3e22352eb95b64880c97f5db5b0c059952df3
Björn Söderqvist <bjorn@greenfield.se>
2017-05-07 10:28:40 +0200

    Unit test anagrams

---
+++ test/anagram.js
@@ -0,0 +1,22 @@
+const dictionary = [
+  'AM', 'APERS', 'APRES', 'ASPER', 'PARES',
+  'PARSE', 'PEARS', 'PRASE', 'PRESA', 'RAPES',
+  'REAPS', 'SPAER', 'SPARE', 'SPEAR'
+];
+
+const {test} = require('tape');
+const anagramFn = require('../lib/anagram');
+
+test('Adds the correct amount of anagrams', assert => {
+  const anagrams = anagramFn(dictionary);
+  const data = anagrams['AEPRS'];
+  assert.equal(13, data.length);
+  assert.end();
+});
+
+test('Doesn´t keep words that have no anagram', assert => {
+  const anagrams = anagramFn(dictionary);
+  const data = anagrams['AM'];
+  assert.equal(undefined, data);
+  assert.end();
+});
